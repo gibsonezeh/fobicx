8e268bcefc65663f0716ffdeca79b309c0084496
gibsonezeh6 <gibsonezeh6@gmail.com>
2025-04-20 11:55:47 +0000

    done

---
+++ fobicx/app/src/test/java/fobicx/MainActivity.java
@@ -0,0 +1,16 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1282656223.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1288287979.
+package fobicx;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
